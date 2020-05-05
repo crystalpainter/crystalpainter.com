2a997b944fb27f8f3af920b6f357b330259a0edc
dach <dach@MacBook-Pro-di-dach.local>
2020-05-04 18:01:30 -0700

    renamed images

---
+++ images/fix.sh
@@ -0,0 +1,226 @@
+#/bin/bash
+echo 'hi'
+
+echo 'hi'
+
+echo 'hi'
+
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=178.jpg.jpg.jpg.jpg' 'artwork_thumb_178.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=259.jpg.jpg.jpg.jpg' 'artwork_thumb_259.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=264.jpg.jpg.jpg.jpg' 'artwork_thumb_264.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=267.jpg.jpg.jpg.jpg' 'artwork_thumb_267.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=278.jpg.jpg.jpg.jpg' 'artwork_thumb_278.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=284.jpg.jpg.jpg.jpg' 'artwork_thumb_284.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=299.jpg.jpg.jpg.jpg' 'artwork_thumb_299.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=306.jpg.jpg.jpg.jpg' 'artwork_thumb_306.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=308.jpg.jpg.jpg.jpg' 'artwork_thumb_308.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=312.jpg.jpg.jpg.jpg' 'artwork_thumb_312.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=315.jpg.jpg.jpg.jpg' 'artwork_thumb_315.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=32.jpg.jpg.jpg.jpg' 'artwork_thumb_32.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=337.jpg.jpg.jpg.jpg' 'artwork_thumb_337.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=347.jpg.jpg.jpg.jpg' 'artwork_thumb_347.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=371.jpg.jpg.jpg.jpg' 'artwork_thumb_371.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=379.jpg.jpg.jpg.jpg' 'artwork_thumb_379.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=421.jpg.jpg.jpg.jpg' 'artwork_thumb_421.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=438.jpg.jpg.jpg.jpg' 'artwork_thumb_438.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=477.jpg.jpg.jpg.jpg' 'artwork_thumb_477.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=77.jpg.jpg.jpg.jpg' 'artwork_thumb_77.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=image&id=81.jpg.jpg.jpg.jpg' 'artwork_thumb_81.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=100.jpg.jpg.jpg.jpg' 'artwork_thumb_100.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=102.jpg.jpg.jpg.jpg' 'artwork_thumb_102.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=103.jpg.jpg.jpg.jpg' 'artwork_thumb_103.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=104.jpg.jpg.jpg.jpg' 'artwork_thumb_104.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=105.jpg.jpg.jpg.jpg' 'artwork_thumb_105.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=108.jpg.jpg.jpg.jpg' 'artwork_thumb_108.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=121.jpg.jpg.jpg.jpg' 'artwork_thumb_121.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=122.jpg.jpg.jpg.jpg' 'artwork_thumb_122.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=131.jpg.jpg.jpg.jpg' 'artwork_thumb_131.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=133.jpg.jpg.jpg.jpg' 'artwork_thumb_133.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=139.jpg.jpg.jpg.jpg' 'artwork_thumb_139.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=145.jpg.jpg.jpg.jpg' 'artwork_thumb_145.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=147.jpg.jpg.jpg.jpg' 'artwork_thumb_147.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=148.jpg.jpg.jpg.jpg' 'artwork_thumb_148.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=156.jpg.jpg.jpg.jpg' 'artwork_thumb_156.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=157.jpg.jpg.jpg.jpg' 'artwork_thumb_157.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=178.jpg.jpg.jpg.jpg' 'artwork_thumb_178.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=18.jpg.jpg.jpg.jpg' 'artwork_thumb_18.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=204.jpg.jpg.jpg.jpg' 'artwork_thumb_204.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=21.jpg.jpg.jpg.jpg' 'artwork_thumb_21.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=224.jpg.jpg.jpg.jpg' 'artwork_thumb_224.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=225.jpg.jpg.jpg.jpg' 'artwork_thumb_225.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=231.jpg.jpg.jpg.jpg' 'artwork_thumb_231.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=235.jpg.jpg.jpg.jpg' 'artwork_thumb_235.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=252.jpg.jpg.jpg.jpg' 'artwork_thumb_252.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=259.jpg.jpg.jpg.jpg' 'artwork_thumb_259.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=262.jpg.jpg.jpg.jpg' 'artwork_thumb_262.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=264.jpg.jpg.jpg.jpg' 'artwork_thumb_264.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=266.jpg.jpg.jpg.jpg' 'artwork_thumb_266.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=267.jpg.jpg.jpg.jpg' 'artwork_thumb_267.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=268.jpg.jpg.jpg.jpg' 'artwork_thumb_268.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=272.jpg.jpg.jpg.jpg' 'artwork_thumb_272.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=278.jpg.jpg.jpg.jpg' 'artwork_thumb_278.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=284.jpg.jpg.jpg.jpg' 'artwork_thumb_284.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=287.jpg.jpg.jpg.jpg' 'artwork_thumb_287.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=289.jpg.jpg.jpg.jpg' 'artwork_thumb_289.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=291.jpg.jpg.jpg.jpg' 'artwork_thumb_291.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=296.jpg.jpg.jpg.jpg' 'artwork_thumb_296.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=299.jpg.jpg.jpg.jpg' 'artwork_thumb_299.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=30.jpg.jpg.jpg.jpg' 'artwork_thumb_30.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=303.jpg.jpg.jpg.jpg' 'artwork_thumb_303.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=305.jpg.jpg.jpg.jpg' 'artwork_thumb_305.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=306.jpg.jpg.jpg.jpg' 'artwork_thumb_306.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=308.jpg.jpg.jpg.jpg' 'artwork_thumb_308.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=311.jpg.jpg.jpg.jpg' 'artwork_thumb_311.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=312.jpg.jpg.jpg.jpg' 'artwork_thumb_312.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=315.jpg.jpg.jpg.jpg' 'artwork_thumb_315.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=319.jpg.jpg.jpg.jpg' 'artwork_thumb_319.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=32.jpg.jpg.jpg.jpg' 'artwork_thumb_32.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=324.jpg.jpg.jpg.jpg' 'artwork_thumb_324.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=325.jpg.jpg.jpg.jpg' 'artwork_thumb_325.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=326.jpg.jpg.jpg.jpg' 'artwork_thumb_326.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=327.jpg.jpg.jpg.jpg' 'artwork_thumb_327.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=331.jpg.jpg.jpg.jpg' 'artwork_thumb_331.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=332.jpg.jpg.jpg.jpg' 'artwork_thumb_332.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=333.jpg.jpg.jpg.jpg' 'artwork_thumb_333.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=339.jpg.jpg.jpg.jpg' 'artwork_thumb_339.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=340.jpg.jpg.jpg.jpg' 'artwork_thumb_340.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=342.jpg.jpg.jpg.jpg' 'artwork_thumb_342.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=344.jpg.jpg.jpg.jpg' 'artwork_thumb_344.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=345.jpg.jpg.jpg.jpg' 'artwork_thumb_345.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=346.jpg.jpg.jpg.jpg' 'artwork_thumb_346.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=347.jpg.jpg.jpg.jpg' 'artwork_thumb_347.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=348.jpg.jpg.jpg.jpg' 'artwork_thumb_348.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=49.jpg.jpg.jpg.jpg' 'artwork_thumb_49.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=50.jpg.jpg.jpg.jpg' 'artwork_thumb_50.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=51.jpg.jpg.jpg.jpg' 'artwork_thumb_51.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=57.jpg.jpg.jpg.jpg' 'artwork_thumb_57.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=60.jpg.jpg.jpg.jpg' 'artwork_thumb_60.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=69.jpg.jpg.jpg.jpg' 'artwork_thumb_69.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=7.jpg.jpg.jpg.jpg' 'artwork_thumb_7.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=74.jpg.jpg.jpg.jpg' 'artwork_thumb_74.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=77.jpg.jpg.jpg.jpg' 'artwork_thumb_77.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=78.jpg.jpg.jpg.jpg' 'artwork_thumb_78.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=79.jpg.jpg.jpg.jpg' 'artwork_thumb_79.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=81.jpg.jpg.jpg.jpg' 'artwork_thumb_81.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=82.jpg.jpg.jpg.jpg' 'artwork_thumb_82.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=85.jpg.jpg.jpg.jpg' 'artwork_thumb_85.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=86.jpg.jpg.jpg.jpg' 'artwork_thumb_86.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=88.jpg.jpg.jpg.jpg' 'artwork_thumb_88.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=89.jpg.jpg.jpg.jpg' 'artwork_thumb_89.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=90.jpg.jpg.jpg.jpg' 'artwork_thumb_90.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=91.jpg.jpg.jpg.jpg' 'artwork_thumb_91.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=92.jpg.jpg.jpg.jpg' 'artwork_thumb_92.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=93.jpg.jpg.jpg.jpg' 'artwork_thumb_93.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=97.jpg.jpg.jpg.jpg' 'artwork_thumb_97.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=98.jpg.jpg.jpg.jpg' 'artwork_thumb_98.jpg';
+echo 'hi'
+mv 'artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_artwork_thumb_showdbpictnew.php?which=thumb&id=99.jpg.jpg.jpg.jpg' 'artwork_thumb_99.jpg';
+echo 'hi'
