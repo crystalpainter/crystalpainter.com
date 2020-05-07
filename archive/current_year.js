21ae0fed1a2224a77ec6b222a67c8952ac9aac61
dach <dach@MacBook-Pro-di-dach.local>
2020-05-07 14:35:49 -0700

    format

--- archive/current_year.js
+++ archive/current_year.js
@@ -1,26 +1,26 @@
+theDate = new Date()
 
+var months = new Array(
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+)
 
+var day = theDate.getDate()
 
-theDate= new Date(); 
+var textdate = 0
 
+if (theDate.getYear() < 2000) textdate = 1900
 
-var months = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
+textdate = theDate.getYear() + textdate
 
-
-var day = theDate.getDate(); 
-
-
-var textdate = 0; 
-
-
-if (theDate.getYear() < 2000) 
-
-
- textdate = 1900; 
-
-
- textdate = (theDate.getYear() + textdate); 
-
-
-document.write(textdate);
-
+document.write(textdate)
